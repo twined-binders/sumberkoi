487d16023efa20a7277513bb2c822265ac17ccee
Fahmi Ahsan <fahmiahsan00@gmail.com>
2023-07-20 21:06:10 +0700

    add transition duration

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -24,10 +24,10 @@
     <div class="bg-slate-100">
         <div class="mt-16 py-5">
             <ul class="uppercase flex gap-10 justify-center font-bold text-xs">
-                <li><a href="{{ route('home') }}">Home</a></li>
-                <li><a href="{{ route('artikel') }}">Artikel</a></li>
-                <li><a href="{{ route('tentang') }}">Tentang Kami</a></li>
-                <li><a href="{{ route('kontak') }}">Kontak</a></li>
+                <li><a href="{{ route('home') }}" class="hover:underline hover:text-[#ff0000] duration-200">Home</a></li>
+                <li><a href="{{ route('artikel') }}" class="hover:underline hover:text-[#ff0000] duration-200">Artikel</a></li>
+                <li><a href="{{ route('tentang') }}" class="hover:underline hover:text-[#ff0000] duration-200">Tentang Kami</a></li>
+                <li><a href="{{ route('kontak') }}" class="hover:underline hover:text-[#ff0000] duration-200">Kontak</a></li>
             </ul>
         </div>
     </div>
@@ -44,10 +44,10 @@
                         <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Dibuat dengan</h2>
                         <ul class="text-gray-500 dark:text-gray-400 font-medium">
                             <li class="mb-4">
-                                <a href="https://laravel.com/" class="hover:underline hover:text-[#FF2D20]">Laravel</a>
+                                <a href="https://laravel.com/" class="hover:underline hover:text-[#FF2D20] duration-200">Laravel</a>
                             </li>
                             <li>
-                                <a href="https://tailwindcss.com/" class="hover:underline hover:text-[#06B6D4]">Tailwind CSS</a>
+                                <a href="https://tailwindcss.com/" class="hover:underline hover:text-[#06B6D4] duration-200">Tailwind CSS</a>
                             </li>
                         </ul>
                     </div>
@@ -55,13 +55,13 @@
                         <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Ikuti Kami</h2>
                         <ul class="text-gray-500 dark:text-gray-400 font-medium">
                             <li class="mb-4">
-                                <a href="https://www.facebook.com/pusatkoiblitar/" class="hover:underline hover:text-[#1877F2]">Facebook</a>
+                                <a href="https://www.facebook.com/pusatkoiblitar/" class="hover:underline hover:text-[#1877F2] duration-200">Facebook</a>
                             </li>
                             <li class="mb-4">
-                                <a href="https://www.instagram.com/sumberkoiblitar/" class="hover:underline hover:text-[#E4405F]">Instagram</a>
+                                <a href="https://www.instagram.com/sumberkoiblitar/" class="hover:underline hover:text-[#E4405F] duration-200">Instagram</a>
                             </li>
                             <li>
-                                <a href="https://wa.me/6281555633367" class="hover:underline hover:text-[#25D366]">Whatsapp</a>
+                                <a href="https://wa.me/6281555633367" class="hover:underline hover:text-[#25D366] duration-200">Whatsapp</a>
                             </li>
                         </ul>
                     </div>
